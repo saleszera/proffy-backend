0df177188375795ac1a8f4e0514910d2f9d60b50
Sales <crowzcoleslaw@gmail.com>
2020-08-08 12:48:06 -0300

    backend desenvolvido na semana nlw

---
+++ src/controllers/ConnectionsController.ts
@@ -0,0 +1,19 @@
+import {Request, Response} from 'express';
+import db from '../database/connection';
+
+export default class ConnectionsController{
+  async create(req: Request, res: Response){
+    const {user_id} = req.body;
+
+    await db('connections').insert({user_id});
+    return res.status(201).json();
+  }
+
+  async index(req: Request, res: Response){
+    const totalConnections = await db('connections').count('* as total');
+
+    const {total} = totalConnections[0];
+
+    return res.status(201).json({total})
+  }
+}
